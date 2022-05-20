57c9011740c938de476aad63ad7ae875edb8f704
shino0423 <shinozaemon0423@gmail.com>
2022-05-20 13:17:41 +0900

    2022/05/20

--- python/python_practice/python_string_method.py
+++ python/python_practice/python_string_method.py
@@ -109,10 +109,78 @@ print("""\
 文字列のメソッドについて
 >メソッドと関数の違い
 呼び出し方が異なること、関数は単体で呼び出し、メソッドは変数やインスタンス
-何らかの値に付与して呼び出している
-
+何らかの値に付与して呼び出している\
+""")
+print("""\
  >メソッド
- 　->
+ 　->クラス内で定義された関数を指す
+ \
+ """)
+print("appendを用いたメソッドの例")
+ex_list = [0,1,2,3]
+print(f"ex_listに4をappendする前ex_list={ex_list}")
+ex_list.append(4)
+print(f"ex_listに4をappendした後ex_list={ex_list}")
+print("joinを用いたメソッド例")
+print('-'.join(["2022","04","23"]))
+
+print(""" 
  >関数
   ->引数に対して、何らかの処理を施し、その結果を戻り値として戻す　
+   ex) print(引数) ->引数を標準出力に表示する
+       len(引数) -> 引数の要素数を表示する\
 """)
+print("消費税込みの価格を返す関数を作成する")
+def cal_tax(a):
+  return int(1.10 * a)
+print(cal_tax(100))
+
+print("####################################################################")
+#よく使うメソッド
+print("My name is shohei. Hi, shoheiの文字列を操作する代表的なメソッド")
+word = "My name is shohei. hi, shohei"
+print(f"findメソッドは引数に入れた文字列が最初から確認して該当する場合に開始位置を戻り値とするメソッドである。", word.find("shohei"))
+print(f"rfindメソッドは引数に入れた文字列が最後から確認して該当する場合に開始位置を戻り値とするメソッドである。", word.rfind("shohei"))
+print(f"coundメソッドは引数に入れた文字列がいくつ含まれているのかを計測し、戻り値として返すメソッド。", word.count("shohei"))
+print(f"capitalizeメソッドは開始の文字だけを大文字にするメソッド", word.capitalize())
+print(f"titleメソッド要素の頭文字を大文字にするメソッド", word.title())
+print(f"upperメソッドは文字列を全て大文字にする", word.upper())
+print(f"lowerメソッドは文字列を全て小文字にする", word.lower())
+print(f"replaceメソッドで文字列を置き換えることもできる", word.replace("shohei", "shinoda"))
+print("####################################################################")
+
+#文字列の代入について
+#pythonにおけるformatメソッドの使い方
+print("'a is {}'.format('a')を実行すると", "a is {}".format("a") )
+print("{}内に代入したい値を代入することができる。ここでは要素数を指定することができる")
+print("a is {0}と{1}と{2}".format("a","b","c"))
+print("インデックスを指定して、出力することもできる")
+print("a is {2}と{1}と{0}".format("一番目の変数","二番目の変数","三番目の変数"))
+print("変数名として、それぞれ指定することもできる")
+print("{name} is {state}.".format(name='shinoda', state='big'))
+#formatメソッドを利用すると代入する文字の型がint型であったとしても、string型の文字列に変換されてしまうことに注意する
+print("python3.6以降では上記の書き方をしなくても以下のように書くことでformatメソッドが利用できるようになった")
+val_1 = "shinoda"
+val_2 = "shohei"
+print(f"a is {val_1}と{val_2}")
+
+
+print("####################################################################")
+#リスト型
+print("[要素1, 要素2, 要素3]の形でリスト型を示す。それぞれの要素にはインデックスが付与されている")
+list_array = ["oyayubi", "hitosashiyubi", "nakayubi", "kusuriyubi", "koyubi"]
+print(f"リストからはじめから三個分の要素を取得する")
+print(f"list_array[0:2]=", list_array[0:2])
+print(f"二番目の要素から最後の要素まで取り出す方法list_array[2:]=", list_array[2:])
+print(f"list_arrayの要素数を数えるlen関数len(list_array)=", len(list_array))
+list_array = ["a", "b", "c", "d", "e", "f", "g"]
+print(f"リスト型list_array={list_array}の操作")
+list_array[0] = 'BB'
+print(f"list_array[0]の変換。list_array[0] = 'BB':", list_array)
+list_array[2:4] = ["VV","GG"]
+print(f"スライスを利用したlist_array[2:4]の変換。list_array[2:4] = ['VV','GG']:", list_array)
+print(f"スライスを利用したリストの操作")
+list_array[2:4] = []
+print(f"list_array[2:4] = []で要素2~4を削除する",list_array)
+list_array[:] = []
+print(f"list_array[:] = []で要素を全て削除する",list_array)
